687c1a4819968a02e10a3c7985ef2d6f8170140a
pjwPro <deljer920@gmail.com>
2019-08-06 01:28:48 +0900

    190806

---
+++ src/main/java/com/pjw/web_base/common/AbstarctDAO.java
@@ -0,0 +1,57 @@
+package com.pjw.web_base.common;
+
+import java.util.List;
+
+import org.mybatis.spring.SqlSessionTemplate;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;;
+
+public class AbstarctDAO {
+	protected Logger log = LoggerFactory.getLogger(this.getClass());
+	
+	@Autowired
+	private SqlSessionTemplate sqlSession;
+	protected void queryId(String queryId) {
+		log.debug("query id =\t"+ queryId);
+	}
+	
+	public Object insert(String queryId, Object params){
+		queryId(queryId);
+		return sqlSession.insert(queryId, params);
+	}
+	
+	public Object update(String queryId, Object params){
+		queryId(queryId);
+		return sqlSession.update(queryId, params);
+	}
+	
+	public Object delete(String queryId, Object params){
+		queryId(queryId);
+		return sqlSession.delete(queryId, params);
+	}
+	
+	public Object selectOne(String queryId){
+		queryId(queryId);
+		return sqlSession.selectOne(queryId);
+	}
+	
+	public Object selectOne(String queryId, Object params){
+		queryId(queryId);
+		return sqlSession.selectOne(queryId, params);
+	}
+	
+	@SuppressWarnings("rawtypes")
+	public List selectList(String queryId){
+		queryId(queryId);
+		return sqlSession.selectList(queryId);
+	}
+	
+	@SuppressWarnings("rawtypes")
+	public List selectList(String queryId, Object params){
+		queryId(queryId);
+		return sqlSession.selectList(queryId,params);
+	}
+
+
+}
