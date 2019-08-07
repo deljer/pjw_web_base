86754196c85c05ffd20294b8088aa616653d1866
pjw <deljer@naver.com>
2019-08-07 09:14:20 +0900

    no message

--- src/main/java/com/pjw/web_base/controller/TestController.java
+++ src/main/java/com/pjw/web_base/controller/TestController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
-import com.pjw.web_base.HomeController;
+
 import com.pjw.web_base.service.TestService;
 
 @Controller
