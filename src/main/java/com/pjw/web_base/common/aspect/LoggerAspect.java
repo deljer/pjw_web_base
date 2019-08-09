2265adf213496066539c64c299f3c9f41e8835d2
pjw <deljer@naver.com>
2019-08-09 16:49:26 +0900

    19-08-09 1.spring aop적용

---
+++ src/main/java/com/pjw/web_base/common/aspect/LoggerAspect.java
@@ -0,0 +1,32 @@
+package com.pjw.web_base.common.aspect;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+
+@Aspect
+public class LoggerAspect {
+	protected Logger log = LogManager.getLogger(this.getClass());
+	static String name = "";
+	static String type = "";
+	
+	@Around("execution(* com..controller.*Controller.*(..)) or execution(* com..serviceimpl.*Impl.*(..)) or execution(* com..*.*Dao.*(..))")
+	public Object logPrint(ProceedingJoinPoint joinPoint) throws Throwable {
+		type = joinPoint.getSignature().getDeclaringTypeName();
+		
+		if (type.indexOf("Controller") > -1) {
+			name = "Controller  \t:  ";
+		}
+		else if (type.indexOf("Service") > -1) {
+			name = "ServiceImpl  \t:  ";
+		}
+		else if (type.indexOf("Dao") > -1) {
+			name = "DAO  \t\t:  ";
+		}
+		log.debug(name + type + "." + joinPoint.getSignature().getName() + "()");
+		return joinPoint.proceed();
+	}
+
+}
