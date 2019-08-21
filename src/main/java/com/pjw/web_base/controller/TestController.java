071e7443ca1e68a88fc3a764d14b6cbdf0543b61
pjw <deljer@naver.com>
2019-08-21 10:54:26 +0900

    19-08-21 1.spring security 기초설

--- src/main/java/com/pjw/web_base/controller/TestController.java
+++ src/main/java/com/pjw/web_base/controller/TestController.java
@@ -4,15 +4,12 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
-
 import org.apache.logging.log4j.Logger;
 import org.apache.logging.log4j.LogManager;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
-
 import com.pjw.web_base.common.util.CommandMap;
 import com.pjw.web_base.service.TestService;
 
@@ -33,16 +30,9 @@ public class TestController {
 	}
 	@RequestMapping(value="/test2.do")
 	public ModelAndView testMapArgumentResolver(CommandMap commandMap) throws Exception{
-		ModelAndView mv = new ModelAndView("");
-		logger.info(mv);
-		if(commandMap.isEmpty() == false){
-			Iterator<Entry<String,Object>> iterator = commandMap.getMap().entrySet().iterator();
-			Entry<String,Object> entry = null;
-			while(iterator.hasNext()){
-				entry = iterator.next();
-				logger.debug("key : "+entry.getKey()+", value : "+entry.getValue());
-			}
-		}
+		ModelAndView mv = new ModelAndView("/home");
+		logger.info("test ");
+		
 		return mv;
 	}
 
